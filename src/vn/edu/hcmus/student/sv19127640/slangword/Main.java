5a76219bf4e63743267a40933208b5a435849dce
hoanghuugiap241 <hoanghuugiap241@gmail.com>
2021-12-15 01:23:54 +0700

    codes: add new slang word, save to history

--- src/vn/edu/hcmus/student/sv19127640/slangword/Main.java
+++ src/vn/edu/hcmus/student/sv19127640/slangword/Main.java
@@ -2,22 +2,17 @@ package vn.edu.hcmus.student.sv19127640.slangword;
 
 public class Main {
     public static void main(String[] args) {
-        // count time
-//        long startTime = System.nanoTime();
-//        long endTime = System.nanoTime();
-//        double timeElapsed = (double) (endTime - startTime) / 1000000000;
-//        System.out.println(timeElapsed + "s");
-        SlangWord slangWord = new SlangWord();
 
-        String[][] s = slangWord.findByDefinition("love");
-        for (int i = 0; i < s.length; i++){
-            System.out.println(s[i][0] + " - "+ s[i][1]);
-        }
         long startTime = System.nanoTime();
-        Object definitions = slangWord.findBySlangWord("ô");
+        SlangWord slangWord = new SlangWord();
+        slangWord.output();
+//        slangWord.addNewSlangWord("giap", "hhgiap| hahagiap|Hoang Huu Giap");
+//        String[][] s = slangWord.findBySlangWord("giap");
         long endTime = System.nanoTime();
         double timeElapsed = (double) (endTime - startTime) / 1000000;
-        System.out.println(timeElapsed + "s");
-        System.out.println(definitions);
+//        for (int i =0;i<s.length;i++){
+//            System.out.println(s[i][0] + " - " + s[i][1] + " - " + s[i][2]);
+//        }
+        System.out.println(timeElapsed + "ms");
     }
 }
--- src/vn/edu/hcmus/student/sv19127640/slangword/SlangWord.java
+++ src/vn/edu/hcmus/student/sv19127640/slangword/SlangWord.java
@@ -13,7 +13,8 @@ public class SlangWord {
     /**
      * attributes
      */
-    private HashMap<String, ArrayList<String>> dictionary;
+    private HashMap<String, HashSet<String>> dictionary;
+    private HashMap<String, HashSet<String>> reverseDictionary;
     private String FILE_NEW_SLANGWORD = "slangword_new.txt";
     private String FILE_ORIGINAL_SLANGWORD = "slang.txt";
     private String FILE_HISTORY = "history.txt";
@@ -22,7 +23,9 @@ public class SlangWord {
      * default constructor
      */
     SlangWord(){
-        this.dictionary = new HashMap<String, ArrayList<String>>();
+        this.dictionary = new HashMap<>();
+        this.reverseDictionary = new HashMap<>();
+//        this.readFromFile();
         File f = new File(FILE_NEW_SLANGWORD);
         if(f.exists() && !f.isDirectory()) {
             // if exits new file => read new file to hashmap
@@ -35,20 +38,40 @@ public class SlangWord {
 
     /**
      * function to add a line from slang.txt to hashmap
-     * @param line
+     * @param line String
      */
     public void addLineToDictionary(String line){
         String word[] = line.split("`");
-        this.dictionary.put(word[0], new ArrayList<String>());
+        this.dictionary.put(word[0], new HashSet<>());
         if (word[1].contains("|")){
             String[] partOfWord = word[1].split("\\|");
             for (int i = 0; i < partOfWord.length; i++){
-                this.dictionary.get(word[0]).add(partOfWord[i].trim());
+                partOfWord[i] = partOfWord[i].trim();
+                this.dictionary.get(word[0]).add(partOfWord[i]);
+                String[] token = partOfWord[i].split("\\W");
+                for (int j = 0; j < token.length; j++){
+                    if (!this.reverseDictionary.containsKey(token[j].toLowerCase())){
+                        this.reverseDictionary.put(token[j].toLowerCase(), new HashSet<>());
+                    }
+                    this.reverseDictionary.get(token[j].toLowerCase()).add(word[0]);
+                }
             }
         }else{
             this.dictionary.get(word[0]).add(word[1].trim());
+            String[] token = word[1].split("\\W");
+            for (int j = 0; j < token.length; j++){
+                if (!this.reverseDictionary.containsKey(token[j].toLowerCase())){
+                    this.reverseDictionary.put(token[j].toLowerCase(), new HashSet<>());
+                }
+                this.reverseDictionary.get(token[j].toLowerCase()).add(word[0]);
+            }
         }
     }
+
+    /**
+     * function to read from file to HashMap
+     * @param filename String
+     */
     public void readFile(String filename){
         try {
             BufferedReader br = new BufferedReader(new FileReader(filename));
@@ -60,6 +83,7 @@ public class SlangWord {
                     break;
                 }
             }
+            this.reverseDictionary.remove("");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -70,48 +94,122 @@ public class SlangWord {
     /**
      * find by slangword
      * @param slangWord String
-     * @return Object
+     * @return String[][]
      */
-    public Object findBySlangWord(String slangWord){
-        return this.dictionary.get(slangWord);
+    public String[][] findBySlangWord(String slangWord){
+        slangWord = slangWord.trim();
+        HashSet<String> values = this.dictionary.get(slangWord);
+        if (values == null)
+            return null;
+        String [][] meanings = new String[values.size()][3];
+        Integer index = 0;
+        for (String value: values){
+            meanings[index][0] = String.valueOf(index + 1);
+            meanings[index][1] = slangWord;
+            meanings[index][2] = value;
+            index++;
+        }
+//        this.saveToHistory(meanings);
+        return meanings;
     }
     /**
      * find slang words by definition
      * @param definition String
-     * @return String Matrix
+     * @return String[][]
      */
     public String[][] findByDefinition(String definition) {
-        List<String> keyList = new ArrayList<>();
-        List<String> meaningList = new ArrayList<>();
-        for (Map.Entry<String, ArrayList<String>> entry : this.dictionary.entrySet()) {
-            List<String> meaning = entry.getValue();
-            for (int i = 0; i < meaning.size(); i++) {
-                if (meaning.get(i).toLowerCase().contains(definition.toLowerCase())) {
-                    keyList.add(entry.getKey());
-                    meaningList.add(meaning.get(i));
-                }
+        String[] tokenizer = definition.trim().toLowerCase().split("\\W");
+        ArrayList<HashSet<String>> valuesList = new ArrayList<>();
+        for (int i = 0; i < tokenizer.length; i++){
+            valuesList.add(this.reverseDictionary.get(tokenizer[i]));
+            if (i > 0){
+                valuesList.get(i).retainAll(valuesList.get(i - 1));
             }
         }
-        int size = keyList.size();
-        String s[][] = new String[size][2];
-        for (int i = 0; i < size; i++) {
-            s[i][0] = keyList.get(i);
-            s[i][1] = meaningList.get(i);
+        HashSet<String> keys = valuesList.get(valuesList.size() - 1);
+        String[][] meanings = new String[keys.size()][3];
+        Integer index = 0;
+
+       for (String key : keys) {
+            meanings[index][0] = String.valueOf(index + 1);
+            meanings[index][1] = key;
+            HashSet<String> values = this.dictionary.get(key);
+            boolean flag = false;
+            for (String value : values){
+                for (String element: tokenizer){
+                    if (value.toLowerCase().contains(element.toLowerCase())){
+                        meanings[index][2] = value;
+                        flag = true;
+                        break;
+                    }
+                }
+                if(flag)
+                    break;
+            }
+            index++;
         }
-        return s;
+        this.saveToHistory(meanings);
+        return meanings;
+    }
+
+    /**
+     * function to write all searched word to history file
+     * @param data String[][]
+     */
+    public void saveToHistory(String[][] data){
+        try {
+            FileWriter fileWriter = new FileWriter(FILE_HISTORY);
+            for (int i = 0; i < data.length; i++){
+                String line = data[i][1] + "`" + data[i][2];
+                fileWriter.write(line + "\n");
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void readFromFile(){
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(
+                                                        new FileInputStream("slag.txt"));
+            this.dictionary = (HashMap<String, HashSet<String>>) objectInputStream.readObject();
+            objectInputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    public void saveToFile(){
+        try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(
+                                                        new BufferedOutputStream(
+                                                                new FileOutputStream("slag.txt")));
+            objectOutputStream.writeObject(this.dictionary);
+            objectOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void addNewSlangWord(String slag, String meanings){
+        String line = slag + "`" + meanings;
+        this.addLineToDictionary(line);
+        this.saveToFile();
     }
     /**
      * print all the word in hashmap
      */
     public void output(){
         System.out.println(this.dictionary);
+        System.out.println("========================================");
+//        System.out.println(this.reverseDictionary);
     }
 
     /**
      * get the dictionary hashmap
      * @return hashmap
      */
-    public HashMap<String, ArrayList<String>> getDictionary() {
+    public HashMap<String, HashSet<String>> getDictionary() {
         return dictionary;
     }
     /**
@@ -134,5 +232,13 @@ public class SlangWord {
      */
     public String getFILE_HISTORY() {
         return FILE_HISTORY;
+    }
+
+    /**
+     * get reverse dictionary
+     * @return HashMap
+     */
+    public HashMap<String, HashSet<String>> getReverseDictionary() {
+        return reverseDictionary;
     }
 }
